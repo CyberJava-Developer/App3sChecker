6dcf35449c4befbc6c89064ed9780d42fc0f86fa
develop-faisal <nurwibowofaisal@gmail.com>
2020-06-15 20:29:20 +0700

    "Mapview Google Api"

--- app/src/main/java/gsm/gsmnetindo/app_3s_checker/ui/dashboard/Fragment_pengawas.kt
+++ app/src/main/java/gsm/gsmnetindo/app_3s_checker/ui/dashboard/Fragment_pengawas.kt
@@ -1,23 +1,23 @@
 package gsm.gsmnetindo.app_3s_checker.ui.dashboard
 
-import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.preference.PreferenceManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import gsm.gsmnetindo.app_3s_checker.R
-import org.osmdroid.api.IMapController
-import org.osmdroid.config.Configuration
-import org.osmdroid.library.BuildConfig
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory
-import org.osmdroid.views.MapView
-import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
-import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 
-class Fragment_pengawas : Fragment() {
+class Fragment_pengawas : Fragment(), OnMapReadyCallback{
+    private lateinit var mMap: GoogleMap
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -25,37 +25,29 @@ class Fragment_pengawas : Fragment() {
     ): View? {
 
         val view = inflater.inflate(R.layout.activity_pengawas, container, false)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
-        val ctx = activity!!.applicationContext
-        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
-        Configuration.getInstance().userAgentValue = BuildConfig.APPLICATION_ID;
-        val map = view.findViewById<MapView>(R.id.mapview)
-
-        map.setUseDataConnection(true)
-        //val map = view.findViewById(R.id.map) as MapView
-        map.setTileSource(TileSourceFactory.MAPNIK)
-        //map.setBuiltInZoomControls(true) //Map ZoomIn/ZoomOut Button Visibility
-        map.setMultiTouchControls(true)
-        val mapController: IMapController
-        mapController = map.controller
-
-        //mapController.zoomTo(14, 1)
-        mapController.setZoom(14)
-
-        val mGpsMyLocationProvider = GpsMyLocationProvider(activity)
-        val mLocationOverlay = MyLocationNewOverlay(mGpsMyLocationProvider, map)
-        mLocationOverlay.enableMyLocation()
-        mLocationOverlay.enableFollowLocation()
-
-        val icon = BitmapFactory.decodeResource(resources, R.drawable.ic_loc)
-        mLocationOverlay.setPersonIcon(icon)
-        map.overlays.add(mLocationOverlay)
-
-        mLocationOverlay.runOnFirstFix {
-            map.overlays.clear()
-            map.overlays.add(mLocationOverlay)
-            mapController.animateTo(mLocationOverlay.myLocation)
-        }
         return view
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        googleMap.uiSettings.isZoomControlsEnabled = true
+        googleMap.setPadding(10,10,10,200)
+
+        mMap = googleMap
+        // Add a marker in surabaya and move the camera
+        val surabaya = LatLng(-7.278030, 112.764384)
+        mMap.addMarker(MarkerOptions().position(surabaya).title("Virus Covid-19").icon(
+            BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(surabaya))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(surabaya, 15f))
+        // Zoom in, animating the camera.
+        mMap.animateCamera(CameraUpdateFactory.zoomIn())
+        // Zoom out to zoom level 10, animating with a duration of 2 seconds.
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15f), 2000, null)
+
+// Add some markers to the map, and add a data object to each marker.
+
     }
 }
