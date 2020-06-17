65e814f069c1d696a8bb454d3fc16b9cb4e48eb6
develop-faisal <nurwibowofaisal@gmail.com>
2020-06-17 14:20:46 +0700

    "Scan data popup"

--- app/src/main/java/gsm/gsmnetindo/app_3s_checker/ui/dashboard/Fragment_QRcode.kt
+++ app/src/main/java/gsm/gsmnetindo/app_3s_checker/ui/dashboard/Fragment_QRcode.kt
@@ -1,10 +1,12 @@
 package gsm.gsmnetindo.app_3s_checker.ui.dashboard
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.budiyev.android.codescanner.CodeScanner
 import com.budiyev.android.codescanner.CodeScannerView
@@ -14,12 +16,17 @@ import gsm.gsmnetindo.app_3s_checker.R
 class Fragment_QRcode : Fragment() {
 
     private lateinit var codeScanner: CodeScanner
+    private lateinit var alertDialog: AlertDialog
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.activity_qrcode, container, false)
+
+        val view = inflater.inflate(R.layout.activity_qrcode, container, false)
+
+        return view
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val scannerView = view.findViewById<CodeScannerView>(R.id.scanner_view)
@@ -27,7 +34,8 @@ class Fragment_QRcode : Fragment() {
         codeScanner = CodeScanner(activity, scannerView)
         codeScanner.decodeCallback = DecodeCallback {
             activity.runOnUiThread {
-                Toast.makeText(activity, it.text, Toast.LENGTH_LONG).show()
+                //Toast.makeText(activity, it.text, Toast.LENGTH_LONG).show()
+                showpopup()
             }
         }
         scannerView.setOnClickListener {
@@ -43,5 +51,28 @@ class Fragment_QRcode : Fragment() {
     override fun onPause() {
         codeScanner.releaseResources()
         super.onPause()
+    }
+
+     fun showpopup(){
+         val inflater: LayoutInflater = this.layoutInflater
+         val dialogView: View = inflater.inflate(R.layout.popup_qrcode_scanner, null)
+
+         val txtcloses = dialogView.findViewById<TextView>(R.id.txtclose)
+         val btncloses: Button = dialogView.findViewById(R.id.btnclose)
+         btncloses.setOnClickListener {
+             alertDialog.dismiss()
+         }
+
+         txtcloses.setOnClickListener{
+             alertDialog.dismiss()
+         }
+
+         val dialogBuilder: AlertDialog.Builder = AlertDialog.Builder(context!!)
+         dialogBuilder.setOnDismissListener { }
+         dialogBuilder.setView(dialogView)
+
+         alertDialog = dialogBuilder.create();
+         alertDialog.window!!.attributes.windowAnimations = R.style.ShapeAppearanceOverlay_MaterialComponents_MaterialCalendar_Window_Fullscreen
+         alertDialog.show()
     }
 }
--- app/src/main/java/gsm/gsmnetindo/app_3s_checker/ui/dashboard/Fragment_pengawas.kt
+++ app/src/main/java/gsm/gsmnetindo/app_3s_checker/ui/dashboard/Fragment_pengawas.kt
@@ -1,14 +1,20 @@
 package gsm.gsmnetindo.app_3s_checker.ui.dashboard
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.DrawableRes
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
@@ -17,6 +23,7 @@ import gsm.gsmnetindo.app_3s_checker.R
 
 class Fragment_pengawas : Fragment(), OnMapReadyCallback{
     private lateinit var mMap: GoogleMap
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,6 +35,7 @@ class Fragment_pengawas : Fragment(), OnMapReadyCallback{
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
+
         return view
     }
 
@@ -38,8 +46,19 @@ class Fragment_pengawas : Fragment(), OnMapReadyCallback{
         mMap = googleMap
         // Add a marker in surabaya and move the camera
         val surabaya = LatLng(-7.278030, 112.764384)
-        mMap.addMarker(MarkerOptions().position(surabaya).title("Virus Covid-19").icon(
-            BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)))
+        val surabaya2 = LatLng(-7.278445, 112.764920)
+
+        //add penanda
+        mMap.addMarker(
+            MarkerOptions().position(surabaya).title("Virus Covid-19")
+                .icon(bitmapDescriptorFromVector
+                    (this.requireContext(), R.drawable.circlemap)))
+
+        mMap.addMarker(
+            MarkerOptions().position(surabaya2).title("Virus Covid-19")
+                .icon(bitmapDescriptorFromVector
+                    (this.requireContext(), R.drawable.circlemap)))
+
         mMap.moveCamera(CameraUpdateFactory.newLatLng(surabaya))
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(surabaya, 15f))
         // Zoom in, animating the camera.
@@ -50,4 +69,31 @@ class Fragment_pengawas : Fragment(), OnMapReadyCallback{
 // Add some markers to the map, and add a data object to each marker.
 
     }
+
+    //change icon circle
+    private fun bitmapDescriptorFromVector(
+        context: Context,
+        @DrawableRes vectorDrawableResourceId: Int
+    ): BitmapDescriptor? {
+        val background =
+            ContextCompat.getDrawable(context, R.drawable.circlemap)
+        background!!.setBounds(0, 0, background.intrinsicWidth, background.intrinsicHeight)
+        val vectorDrawable = ContextCompat.getDrawable(context, vectorDrawableResourceId)
+        vectorDrawable!!.setBounds(
+            40,
+            20,
+            vectorDrawable.intrinsicWidth + 40,
+            vectorDrawable.intrinsicHeight + 20
+        )
+        val bitmap = Bitmap.createBitmap(
+            background.intrinsicWidth,
+            background.intrinsicHeight,
+            Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(bitmap)
+        background.draw(canvas)
+        vectorDrawable.draw(canvas)
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
+    }
+
 }
