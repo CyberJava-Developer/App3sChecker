fd91ada2849670aeb2df3fb355662dddf0dc4a9c
diolan12 <dio_lantief21@outlook.com>
2020-09-14 21:30:20 +0700

    -added avatar load

--- app/src/main/java/gsm/gsmnetindo/app_3s_checker/ui/dashboard/Fragment_akun.kt
+++ app/src/main/java/gsm/gsmnetindo/app_3s_checker/ui/dashboard/Fragment_akun.kt
@@ -1,6 +1,7 @@
 package gsm.gsmnetindo.app_3s_checker.ui.dashboard
 
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -8,8 +9,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.load.model.GlideUrl
+import com.bumptech.glide.load.model.LazyHeaders
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import com.bumptech.glide.signature.ObjectKey
 import gsm.gsmnetindo.app_3s_checker.R
 import gsm.gsmnetindo.app_3s_checker.internal.ScopedFragment
+import gsm.gsmnetindo.app_3s_checker.internal.Secret
+import gsm.gsmnetindo.app_3s_checker.internal.glide.GlideApp
 import gsm.gsmnetindo.app_3s_checker.ui.login.loginverification
 import gsm.gsmnetindo.app_3s_checker.ui.viewmodel.AccountViewModel
 import gsm.gsmnetindo.app_3s_checker.ui.viewmodel.AccountViewModelFactory
@@ -34,16 +44,18 @@ class Fragment_akun : ScopedFragment(), KodeinAware {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         progressktp.visibility = View.GONE
-        accountViewModel = ViewModelProvider(this, accountViewModelFactory).get(AccountViewModel::class.java)
+        accountViewModel =
+            ViewModelProvider(this, accountViewModelFactory).get(AccountViewModel::class.java)
         account_button_logout.setOnClickListener {
             logout()
         }
         bindUI()
         syncDetail()
     }
-    private fun logout(){
+
+    private fun logout() {
         accountViewModel.logout()
-        if (activity != null){
+        if (activity != null) {
             Intent(activity, loginverification::class.java).apply {
                 flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                 startActivity(this)
@@ -51,20 +63,60 @@ class Fragment_akun : ScopedFragment(), KodeinAware {
             }
         }
     }
+
     private fun bindUI() {
-        accountViewModel.detail.observe(viewLifecycleOwner, Observer {
+        accountViewModel.detail.observe(viewLifecycleOwner, {
             Log.i("fragment detail", "$it")
             txtname.text = it.detail.name
             nomortelepon.text = "+${it.account.phone}"
             txtlahir.text = "${it.detail.bornPlace}, ${it.detail.bornDate}"
-
+            loadAvatar(ObjectKey(it.account.avatar))
         })
     }
+
     private fun syncDetail() = launch {
         try {
             accountViewModel.syncDetail()
         } catch (e: Exception) {
             Log.e("fetchDetail", e.message, e)
         }
+    }
+
+    private fun loadAvatar(key: ObjectKey) {
+        val url =
+            "${Secret.baseApi()}${Secret.apiVersion()}/${accountViewModel.getPhonePref()}/avatar.jpg"
+        val glideUrl = GlideUrl(
+            url,
+            LazyHeaders.Builder()
+                .addHeader("Api-Key", Secret.apiKey())
+                .addHeader("Authorization", "Bearer ${accountViewModel.getToken().value}")
+                .build()
+        )
+        GlideApp.with(this)
+            .load(glideUrl)
+            .signature(key)
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return e != null
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    target.apply { account_avatar.setImageDrawable(resource) }
+                    return resource != null
+                }
+
+            })
+            .into(account_avatar)
     }
 }
