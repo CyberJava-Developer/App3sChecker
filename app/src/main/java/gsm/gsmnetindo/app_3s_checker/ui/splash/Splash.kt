f96f81fefcf11e41dd9ac774f1858a3ac1b8f580
develop-faisal <nurwibowofaisal@gmail.com>
2020-12-20 01:36:32 +0700

    Renew Design Splash

--- app/src/main/java/gsm/gsmnetindo/app_3s_checker/ui/splash/Splash.kt
+++ app/src/main/java/gsm/gsmnetindo/app_3s_checker/ui/splash/Splash.kt
@@ -8,7 +8,9 @@ import android.view.WindowManager
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import cn.pedant.SweetAlert.SweetAlertDialog
@@ -21,6 +23,7 @@ import gsm.gsmnetindo.app_3s_checker.ui.login.LoginActivity
 import gsm.gsmnetindo.app_3s_checker.ui.main.MainActivity
 import gsm.gsmnetindo.app_3s_checker.ui.viewmodel.AccountViewModel
 import gsm.gsmnetindo.app_3s_checker.ui.viewmodel.AccountViewModelFactory
+import kotlinx.android.synthetic.main.activity_splash.*
 import kotlinx.coroutines.launch
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.closestKodein
@@ -40,22 +43,27 @@ class Splash : ScopedActivity(), KodeinAware {
     lateinit var topanim:Animation
     lateinit var buttomanim:Animation
     lateinit var image:ImageView
-    lateinit var logotext:ImageView
+    lateinit var logotext:TextView
     lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
         setContentView(R.layout.activity_splash)
         splashViewModel = ViewModelProvider(this, splashViewModelFactory).get(SplashViewModel::class.java)
         accountViewModel = ViewModelProvider(this, accountViewModelFactory).get(AccountViewModel::class.java)
-
+        val versionCode = BuildConfig.VERSION_NAME
+        version.text = "Versi: ${versionCode}"
         topanim = AnimationUtils.loadAnimation(this, R.anim.top_animation)
         buttomanim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation)
         image = findViewById(R.id.Logo)
-        logotext = findViewById(R.id.textlogo)
+        logotext = findViewById(R.id.version)
 
         image.animation = topanim
         logotext.animation = buttomanim
+
 
         handler = Handler()
         handler.postDelayed({
@@ -63,12 +71,12 @@ class Splash : ScopedActivity(), KodeinAware {
 //            val intent = Intent(this, IntroActivity::class.java)
 //            startActivity(intent)
 //            finish()
-        }, 5000)
+        }, 3000)
     }
     private fun checkNetwork(){
         if (splashViewModel.isOnline().not()){
             alerndialog()
-            Toast.makeText(this,"tidak ada koneksi internet", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "tidak ada koneksi internet", Toast.LENGTH_LONG).show();
             //finish()
         } else {
             checkUpdate()
@@ -93,12 +101,13 @@ class Splash : ScopedActivity(), KodeinAware {
                 if (currentVersion < it.appVersionCode) {
                     SweetAlertDialog(this@Splash, SweetAlertDialog.SUCCESS_TYPE).apply {
                         setTitleText("Versi terbaru telah rilis")
-                        .setContentText("Mohon update ${it.appName} ke versi ${it.appVersionName}")
-                        .setConfirmText("OK")
-                        .setConfirmClickListener { sDialog -> sDialog.dismissWithAnimation()
-                            exitProcess(0)
-                        }
-                        .show()
+                            .setContentText("Mohon update ${it.appName} ke versi ${it.appVersionName}")
+                            .setConfirmText("OK")
+                            .setConfirmClickListener { sDialog ->
+                                sDialog.dismissWithAnimation()
+                                exitProcess(0)
+                            }
+                            .show()
                     }
 
                 } else {
@@ -119,7 +128,12 @@ class Splash : ScopedActivity(), KodeinAware {
                     alern()
                 }
                 else -> {
-                    Toast.makeText(this@Splash, e.message, Toast.LENGTH_LONG).show()
+//                    Toast.makeText(this@Splash, e.message, Toast.LENGTH_LONG).show()
+                    Toast.makeText(
+                        this@Splash,
+                        "Tidak bisa menyambung ke server, coba beberapa saat lagi",
+                        Toast.LENGTH_LONG
+                    ).show()
                 }
             }
             //finish()
@@ -141,7 +155,7 @@ class Splash : ScopedActivity(), KodeinAware {
     private fun isFirstTime() {
         try {
             splashViewModel.isFirst().observe(this, Observer {
-                if (it){
+                if (it) {
                     // to intro
                     Intent(this, IntroActivity::class.java).apply {
                         flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
@@ -171,7 +185,7 @@ class Splash : ScopedActivity(), KodeinAware {
     }
     private fun isLoggedIn(){
         accountViewModel.isLoggedIn().observe(this, Observer {
-            if (it){
+            if (it) {
                 toMain()
             } else {
                 toLogin()
@@ -187,7 +201,11 @@ class Splash : ScopedActivity(), KodeinAware {
                     finish()
                 }
             } else {
-                Toast.makeText(this@Splash, "anda tidak memiliki izin untuk login", Toast.LENGTH_LONG).show()
+                Toast.makeText(
+                    this@Splash,
+                    "anda tidak memiliki izin untuk login",
+                    Toast.LENGTH_LONG
+                ).show()
                 accountViewModel.logout()
                 toLogin()
             }
